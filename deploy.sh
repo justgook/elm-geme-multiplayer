2344acb38d82b473ddeb6a6de9d4fc869a2e20cc
Romāns Potašovs <justgook@gmail.com>
2021-01-04 15:50:38 +0200

    New deployment script (1.0.0)

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,36 @@ set -eu
 
 repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
-main_branch="release"
+#main_branch="release"
 target_branch="gh-pages"
 build_dir="target"
+temp_dir="$GITHUB_WORKSPACE/temp"
 
-cd "$GITHUB_WORKSPACE"
+#Get last tag
+current_tag="$(git describe --abbrev=0)"
 
+mkdir -p "$temp_dir/$current_tag"
+cd "$temp_dir"
+git init .
+
+#fetch data
+git remote add "$remote_name" "$repo_uri"
+git fetch "$remote_name" "$target_branch"
+git checkout "$target_branch"
+
+#Clean up current snapshots
+rm -rf ./*-snap
+
+#Move new build
+mv "$GITHUB_WORKSPACE/$build_dir" "$temp_dir/$current_tag"
+
+#Update redirect url
+sed -i'' -e "s/\/[^\/]*\/browser.html/\/$current_tag\/browser.html/g" 404.html
+
+#Commit and deploy
 git config user.name "$GITHUB_ACTOR"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
-
-git checkout "$target_branch"
-git rebase "${remote_name}/${main_branch}"
-
-./bin/build "$build_dir"
-git add "$build_dir"
-
-git commit -m "updated GitHub Pages"
+git commit -am "Deploy: $(git log --oneline --format=%B -n 1 HEAD | head -n 1)"
 if [ $? -ne 0 ]; then
     echo "nothing to commit"
     exit 0
